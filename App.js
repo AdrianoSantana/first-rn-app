4f94567d0e75564cc3025bbb675d6b1529578e33
Usuario <ans3@cin.ufpe.br>
2024-03-23 07:43:56 -0300

    feat: adding goals to a list

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, View, Text, Button } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [inputGoalText, setInputdGoalText] = useState('')
+  const [listOfGoals, setListOfGoals] = useState([])
+  
+  function goalInputHandler(inputText) {
+    setInputdGoalText(inputText)
+  }
+
+  function addGoalHandler() {
+    setListOfGoals((currentList) => {
+      return [...currentList, inputGoalText]
+    })
+    setInputdGoalText('')
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput 
+          style={styles.inputText} 
+          placeholder='O que você precisa fazer hoje ?'
+          onChangeText={goalInputHandler}
+          value={inputGoalText}
+        />
+        <Button 
+          title='Add Goal'
+          onPress={addGoalHandler} 
+        />
+      </View>
+      <View style={styles.goalsContainer}>
+        {listOfGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
+      </View>
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 50,
+    paddingHorizontal: 16,
+    borderColor: 'yellow',
+    borderWidth: 2,
+    flex: 1
   },
+  
+  goalsContainer: {
+    borderColor: 'red',
+    borderWidth: 2,
+    flex: 5
+  },
+  
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 4,
+    borderBottomColor: '#d2d2d2',
+    flex: 1,
+  },
+
+  inputText: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '75%',
+    marginRight: 8,
+    padding: 8
+  }
 });
+
